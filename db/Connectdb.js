3204d9c9455e9e67cede2920ea3b8134f8c99afd
ankitjeevijay <ankitjeevijay@gmail.com>
2023-04-12 23:07:27 +0530

    first

--- db/Connectdb.js
+++ db/Connectdb.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
-const url = "mongodb://127.0.0.1:27017/BlogProject"
+//const url = "mongodb://127.0.0.1:27017/BlogProject"
 
-
+const live_Url = "mongodb+srv://ankitjeevijay:ankit123@cluster0.x0v6u8p.mongodb.net/blog"
 
 
 const ConnectDB = ()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_Url)
+    //return mongoose.connect(url)
+    
 
     .then(()=>{
         console.log("Database connected....")
